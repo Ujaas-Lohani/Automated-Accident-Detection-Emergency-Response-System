ba6552c6f7d48b5ce0e0a45af9af93722c8b1ad6
ujaas lohani <ujaaslohani261104@gmail.com>
2024-07-10 09:46:44 +0530

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+   loginObj:any={
+  "mail":""
+   }
+    APIURL="http://localhost:8000/";
+    http=inject(HttpClient);
+    onLogin(){
+      debugger;
+      let body=new FormData();
+      body.append('mail',this.loginObj.mail);
+      this.http.post("http://localhost:8000/get_email", body).subscribe((res: any) => {
+        debugger;
+        alert(res)
+    })
+    }
 }
